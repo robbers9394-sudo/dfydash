14edab0b2603c46b358b5bd677648d5b008d75f7
robbers9394-sudo <robbers9394@gmail.com>
2026-03-26 21:05:59 +0900

    Update dfysupabase-config.js

--- dfysupabase-config.js
+++ dfysupabase-config.js
@@ -1,4 +1,5 @@
 window.DFY_APP_CONFIG = window.DFY_APP_CONFIG || {
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://qhjsjdaeldaxyfgjhwvq.supabase.co",
+  supabaseAnonKey: "sb_publishable_Oz5EjFVcdWXtD5iPVYGDag_JP29Z0Uq"
+
 };
